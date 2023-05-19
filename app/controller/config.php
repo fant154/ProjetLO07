4179d9c80a2a6003754634480876162e3e544f52
Olivier Besnard <olivierbesnardvii@gmail.com>
2023-05-19 20:36:43 +0200

    Ajout config

---
+++ app/controller/config.php
@@ -0,0 +1,46 @@
+
+<!-- ----- debut config -->
+<?php
+
+// Utile pour le débugage car c'est un interrupteur pour les echos et print_r.
+if (!defined('DEBUG')) {
+    define('DEBUG', FALSE);
+}
+
+// ===============
+// Configuration de la base de données sur dev-isi
+$dsn = 'mysql:dbname=besnardo;host=localhost;charset=utf8';
+$username = 'besnardo';
+$password = 'pPE9soPs';
+
+if (!defined('LOCAL')) {
+    define('LOCAL', TRUE);
+}
+
+if (LOCAL) {
+    // Configuration de la base de données sur localhost
+    $dsn = 'mysql:dbname=besnardo;host=localhost;port=3307;charset=utf8';
+    $username = 'besnardo';
+    $password = 'besnardo';
+}
+ 
+// chemin absolu vers le répertoire du projet SUR DEV-ISI 
+$root = dirname(dirname(__DIR__)) . "/";
+
+
+if (DEBUG) {
+ echo ("<ul>");
+ echo (" <li>dsn = $dsn</li>");
+ echo (" <li>username = $username</li>");
+ echo (" <li>password = $password</li>");
+ echo ("<li>---</li>");
+ echo (" <li>root = $root</li>");
+
+ echo ("</ul>");
+}
+?>
+
+<!-- ----- fin config -->
+
+
+
